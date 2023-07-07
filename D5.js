475a679583e9e7891b103fa9440aea0c562c6156
Elmeleh <alaa.elmeleh@outlook.it>
2023-07-07 23:54:59 +0200

    02-D5 FINITO

--- D5.js
+++ D5.js
@@ -10,7 +10,9 @@ REGOLE
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
 const pets = ["dog", "cat", "hamster", "redfish"];
-console.log(pets);
+for (let i = 0; i < pets.length; i++) {
+  console.log(pets[i]);
+}
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
@@ -83,7 +85,7 @@ console.log(cars);
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = [];
-for (i = 0; i < cars.length; i++) {
+for (let i = 0; i < cars.length; i++) {
   let firstElement = cars[i].trims[0];
   justTrims.push(firstElement);
 }
@@ -92,8 +94,8 @@ console.log(justTrims);
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
-for (i = 0; i < cars.length; i++) {
-  if (cars[i].color.startsWith("b", 0)) {
+for (let i = 0; i < cars.length; i++) {
+  if (cars[i].color[0] === "b") {
     console.log("Fizz");
   } else {
     console.log("Buzz");
@@ -104,10 +106,101 @@ for (i = 0; i < cars.length; i++) {
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105];
+let i = -1;
 
+while (i < numericArray.length) {
+  i++;
+  console.log(numericArray[i]);
+  if (numericArray[i] === 32) {
+    break;
+  }
+}
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ["g", "n", "u", "z", "d"];
+const numberArray = [];
+for (let i = 0; i < charactersArray.length; i++) {
+  switch (charactersArray[i]) {
+    case "a":
+      numberArray.push(1);
+      break;
+    case "b":
+      numberArray.push(2);
+      break;
+    case "c":
+      numberArray.push(3);
+      break;
+    case "d":
+      numberArray.push(4);
+      break;
+    case "e":
+      numberArray.push(5);
+      break;
+    case "f":
+      numberArray.push(6);
+      break;
+    case "g":
+      numberArray.push(7);
+      break;
+    case "h":
+      numberArray.push(8);
+      break;
+    case "i":
+      numberArray.push(9);
+      break;
+    case "j":
+      numberArray.push(10);
+      break;
+    case "k":
+      numberArray.push(11);
+      break;
+    case "l":
+      numberArray.push(12);
+      break;
+    case "m":
+      numberArray.push(13);
+      break;
+    case "n":
+      numberArray.push(14);
+      break;
+    case "o":
+      numberArray.push(15);
+      break;
+    case "p":
+      numberArray.push(16);
+      break;
+    case "q":
+      numberArray.push(17);
+      break;
+    case "r":
+      numberArray.push(18);
+    case "s":
+      numberArray.push(19);
+      break;
+    case "t":
+      numberArray.push(20);
+      break;
+    case "u":
+      numberArray.push(21);
+      break;
+    case "v":
+      numberArray.push(22);
+      break;
+    case "w":
+      numberArray.push(23);
+      break;
+    case "x":
+      numberArray.push(24);
+      break;
+    case "y":
+      numberArray.push(25);
+      break;
+    case "z":
+      numberArray.push(26);
+      break;
+  }
+  console.log(numberArray[i]);
+}
